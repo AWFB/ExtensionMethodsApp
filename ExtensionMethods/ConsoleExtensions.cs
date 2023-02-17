12bc645aa812ed9345855fc73a1fd984ce80026c
Antony Beardsmore <sider@me.com>
2023-02-17 14:11:01 +0000

    Add project files.

---
+++ ExtensionMethods/ConsoleExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    public static class ConsoleExtensions
+    {
+        public static string RequestString(this string message)
+        {
+            string output = "";
+
+            while (string.IsNullOrWhiteSpace(output))
+            {
+                Console.Write(message);
+                output = Console.ReadLine();
+            }
+
+            return output;
+        }
+
+        // For converting console readline string to int
+        public static int RequestInt(this string message)
+        {
+            return message.RequestInt(false);
+        }
+
+        public static int RequestInt(this string message, int minValue, int maxValue)
+        {
+            return message.RequestInt(true, minValue, maxValue);
+        }
+
+        private static int RequestInt(this string message, bool useMinMax, int minValue = 0, int maxValue = 0) // min value and max value can be optional parameters
+        {
+            int output = 0;
+            bool isValidInt = false;
+            bool withinValidRange = true;
+
+            while (isValidInt == false || withinValidRange == false)
+            {
+                Console.Write(message);
+                isValidInt = int.TryParse(Console.ReadLine(), out output);
+
+                if (useMinMax == true)
+                {
+                    withinValidRange = (output >= minValue && output <= maxValue);
+                    //if (output >= minValue && output <= maxValue)
+                    //{
+                    //    withinValidRange = true;
+                    //}
+                    //else
+                    //{
+                    //    withinValidRange = false;
+                    //}
+                }
+            }
+            return output;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel();
+
+            person.FirstName = "What is your first name: ".RequestString();
+            person.LastName = "What is your last name: ".RequestString();
+
+            // Either way works
+            person.Age = "What is your age: ".RequestInt(0, 130);
+            person.Age = ConsoleExtensions.RequestInt("What is your age: ", 0, 130);
+
+            // Print result
+            Console.WriteLine($"Hello {person.FirstName} {person.LastName}, you are {person.Age} years old");
+            Console.ReadLine();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
